c5ef959c0674268003a387fc7e8aacc256d95242
Adam Carlile <adam@benchmedia.co.uk>
2009-06-30 20:43:31 +0100

    Added profiles and profile index

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090126212539) do
+ActiveRecord::Schema.define(:version => 20090625212402) do
+
+  create_table "assets", :force => true do |t|
+    t.string   "title"
+    t.string   "caption"
+    t.integer  "attatchment_id"
+    t.string   "attatchment_type"
+    t.string   "asset_file_name"
+    t.string   "asset_content_type"
+    t.integer  "asset_file_size"
+    t.datetime "asset_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "attatchments", :force => true do |t|
+    t.integer  "asset_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "enquiries", :force => true do |t|
     t.datetime "created_at"
@@ -32,6 +51,19 @@ ActiveRecord::Schema.define(:version => 20090126212539) do
     t.string   "referal"
   end
 
+  create_table "events", :force => true do |t|
+    t.string   "title"
+    t.datetime "start"
+    t.datetime "finish"
+    t.string   "intro"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "location"
+    t.string   "lng"
+    t.string   "lat"
+  end
+
   create_table "file_uploads", :force => true do |t|
     t.string   "file_file_name"
     t.string   "file_content_type"
@@ -44,11 +76,12 @@ ActiveRecord::Schema.define(:version => 20090126212539) do
     t.string   "alt"
     t.string   "image_file_name"
     t.string   "image_content_type"
-    t.float    "resize",     :default => 1.0
+    t.float    "resize",             :default => 1.0
     t.integer  "crop_w"
     t.integer  "crop_h"
     t.integer  "crop_x"
     t.integer  "crop_y"
+    t.integer  "page_id"
   end
 
   create_table "pages", :force => true do |t|
@@ -66,20 +99,34 @@ ActiveRecord::Schema.define(:version => 20090126212539) do
     t.string   "slug_path"
     t.string   "title_path"
     t.boolean  "locked",           :default => false, :null => false
-    t.boolean  "published",          :default => false, :null => false
+    t.boolean  "published",        :default => false, :null => false
     t.datetime "publish_date"
     t.string   "url"
     t.text     "intro"
     t.text     "body"
   end
 
+  create_table "promos", :force => true do |t|
+    t.integer  "page_id"
+    t.string   "title"
+    t.string   "override_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.text     "intro"
+    t.integer  "position"
+  end
+
   create_table "users", :force => true do |t|
-    t.boolean  "admin", :default => false
+    t.boolean  "admin",                                    :default => false
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
-    t.string   "crypted_password",          :limit => 128, :default => "", :null => false
-    t.string   "salt",                      :limit => 128, :default => "", :null => false
+    t.string   "crypted_password",          :limit => 128, :default => "",    :null => false
+    t.string   "salt",                      :limit => 128, :default => "",    :null => false
     t.string   "remember_token"
     t.string   "remember_token_expires_at"
     t.datetime "created_at"
@@ -87,8 +134,8 @@ ActiveRecord::Schema.define(:version => 20090126212539) do
     t.string   "persistence_token"
     t.string   "single_access_token"
     t.string   "perishable_token"
-    t.integer  "login_count",                              :default => 0,  :null => false
-    t.integer  "failed_login_count",                       :default => 0,  :null => false
+    t.integer  "login_count",                              :default => 0,     :null => false
+    t.integer  "failed_login_count",                       :default => 0,     :null => false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
